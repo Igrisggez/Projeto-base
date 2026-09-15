2ed1b19ce80c08d6eaa04f37c0ae1aa65021ea0d
Igrisggez <igris.ggez@gmail.com>
2026-09-15 17:12:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// Parte 1-Cadastro
+alert("Bem vindo ao site!");
+let nome = prompt("Qual seu nome?");
+let idade = parseInt(prompt("Qual sua idade?"));
+
+// Lista de convidados
+let convidados = [];
+
+// Validação com Condição
+if (idade <= 17) {
+  alert("Voce deve esperar completar 18 anos");
+} else {
+  convidados.push(nome);
+}
+// Repetição
+let resposta = prompt("Mais alguma conta?")
+  .toLowerCase()
+  .trim()
+  .normalize("NFD")
+  .replace(/[\u0300-\u036f]/g, "");
+while (resposta == "sim") {
+  nome = prompt("Qual seu nome?");
+  idade = parseInt(prompt("Qual sua idade?"));
+  if (idade <= 17) {
+    alert("Espere ser de maior!");
+  } else {
+    convidados.push(nome);
+  }
+  resposta = prompt("Mais alguma conta?")
+    .toLowerCase()
+    .trim()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+}
+function contarConvidados(convidados) {
+  return convidados.length;
+}
+convidados.sort();
+alert(`convidados finais: ${convidados.join(", ")} = ${contarConvidados(convidados)}`);
